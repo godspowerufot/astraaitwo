0aae18e0def9839bae9a871e9cae317ef325636e
godspowerufot <ufotgodspower8@gmail.com>
2025-05-27 07:41:18 +0100

    changes

--- src/app/page.js
+++ src/app/page.js
@@ -174,14 +174,16 @@ const [tokensWithPrices, setTokensWithPrices] = useState([]);
   return (
     <div className="min-h-screen bg-black text-white flex flex-col items-center">
       <header className="w-full flex justify-center items-center bg-gray-900 p-4 text-2xl font-bold uppercase tracking-widest border-b border-gray-700">
-        ASTRAEUS AI
+    <img src="/hedge.png" alt='hedge' className='w-20 h-20'/> HEDGE
       </header>
       <div className="w-full bg-gray-800 py-2 mb-4 overflow-hidden border border-gray-700 rounded">
         <div
           className="flex font-sans items-center gap-8 animate-marquee whitespace-nowrap"
           style={{ animationDuration: "100s" }}
         >
-          {tokensWithPrices.length === 0 && <span>Loading tokens...</span>}
+
+    <img src="/Heddge.png" alt='hedge' className='w-20 h-20'/> 
+          {/* {tokensWithPrices.length === 0 && <span>Loading tokens...</span>}
           {tokensWithPrices.map((token, idx) => (
             <div key={idx} className="flex gap-3 ml-4">
               <img
@@ -195,7 +197,7 @@ const [tokensWithPrices, setTokensWithPrices] = useState([]);
                 </span>
               </div>
             </div>
-          ))}
+          ))} */}
         </div>
       </div>
       <div className="flex gap-2 my-4">
@@ -298,6 +300,15 @@ Craft nft avaters        </button>
           )}
         </div>
       )}
+
+
+
+
+
+      <div>
+          <img src="/x.png" alt='hedge' className='w-10 h-10'/> 
+
+      </div>
     </div>
   );
 }
